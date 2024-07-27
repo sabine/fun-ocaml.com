349b0046709ee7db704f0686ccdff81f6acb873e
sabine <6594573+sabine@users.noreply.github.com>
2024-07-27 19:20:08 +0200

    (WIP) towards session pages

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,34 @@
+let write_file path content =
+  let parent_dir =
+    Fpath.of_string path |> Result.get_ok |> Fpath.parent |> Fpath.to_string
+  in
+  Sys.command ("mkdir -p " ^ parent_dir) |> ignore;
+  let oc = open_out path in
+  Printf.fprintf oc "%s" content;
+  close_out oc
+
 let render_homepage () =
   let html =
     Templates.Home.make () |> JSX.render |> Printf.sprintf "<!doctype html>%s"
   in
-  let oc = open_out "output/index.html" in
-  Printf.fprintf oc "%s" html;
-  close_out oc
+  write_file "output/index.html" html
+
+let render_session_page (s : Data.Sessions.t) =
+  let html =
+    Templates.Session.render s |> JSX.render
+    |> Printf.sprintf "<!doctype html>%s"
+  in
+  write_file ("output/2024/" ^ s.slug ^ "/index.html") html
 
 let render_privacy_policy () =
   let html =
     Templates.Privacy.make () |> JSX.render
     |> Printf.sprintf "<!doctype html>%s"
   in
-  let oc = open_out "output/privacy/index.html" in
-  Printf.fprintf oc "%s" html;
-  close_out oc
+  write_file "output/privacy/index.html" html
 
 let () =
   render_homepage ();
-  render_privacy_policy ()
+  render_privacy_policy ();
+  Data.Sessions.all
+  |> List.iter (fun (s : Data.Sessions.t) -> render_session_page s)
