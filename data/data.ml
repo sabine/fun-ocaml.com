1e831fde04328d990d4437705a827071f53cc914
sabine <6594573+sabine@users.noreply.github.com>
2024-12-17 18:32:58 +0100

    new section recommended events, sponsor links

--- data/data.ml
+++ data/data.ml
@@ -80,6 +80,19 @@ module People = struct
     List.find (fun (speaker : t) -> speaker.slug = slug) all
 end
 
+module RecommendedEvents = struct
+  type event = { name : string; logo_url : string; url : string }
+
+  let all : event list =
+    [
+      {
+        name = "BOBKonf";
+        logo_url = "/img/recommended-events/bobkonf.png";
+        url = "https://bobkonf.de";
+      };
+    ]
+end
+
 module Sessions = struct
   type metadata = {
     slug : string;
@@ -116,6 +129,7 @@ module Sessions = struct
         | "workshop" -> Workshop
         | t -> failwith ("session kind not recognized: " ^ t));
     }
+
   let decode data =
     let metadata = metadata_list_of_yaml data in
     Result.map (List.map of_metadata) metadata
@@ -127,53 +141,60 @@ end
 
 let speakers = People.all
 
-module Sponsors = struct 
-  type sponsor = {
-    name: string;
-    logo_url: string;
-  }
+module Sponsors = struct
+  type sponsor = { name : string; logo_url : string; url : string }
 
-  let all: sponsor list  =
+  let all : sponsor list =
     [
-      { name = "Tarides"; logo_url= "/img/tarides.webp" };
-      { name = "Ahrefs"; logo_url= "/img/ahrefs.svg" };
-      { name = "Jane Street"; logo_url= "/img/jane_street.svg" };
-      { name = "LightSource"; logo_url= "/img/lightsource.svg"};
-      { name = "Hyper"; logo_url="/img/hyper.svg"}
+      {
+        name = "Tarides";
+        logo_url = "/img/tarides.webp";
+        url = "https://tarides.com";
+      };
+      {
+        name = "Ahrefs";
+        logo_url = "/img/ahrefs.svg";
+        url = "https://ahrefs.com";
+      };
+      {
+        name = "Jane Street";
+        logo_url = "/img/jane_street.svg";
+        url = "https://www.janestreet.com/";
+      };
+      {
+        name = "LightSource";
+        logo_url = "/img/lightsource.svg";
+        url = "https://lightsource.ai/";
+      };
+      {
+        name = "Hyper";
+        logo_url = "/img/hyper.svg";
+        url = "https://hyper.systems";
+      };
     ]
-
 end
 
 module Schedule = struct
-  type event = {
-    event: string;
-    colspan: int option;
-    rowspan: int option;
-  }
+  type event = { event : string; colspan : int option; rowspan : int option }
   [@@deriving of_yaml]
-
 
   type schedule_row = {
-    time: string;
-    yellow_giant: event option;
-    apollo: event option;
-    hangout_hub_room: event option;
-    hangout_hub: event option;
+    time : string;
+    yellow_giant : event option;
+    apollo : event option;
+    hangout_hub_room : event option;
+    hangout_hub : event option;
   }
   [@@deriving of_yaml]
 
-
-  type schedule = {
-    date: string;
-    schedule: schedule_row list;
-  }
+  type schedule = { date : string; schedule : schedule_row list }
   [@@deriving of_yaml]
 
-
-  let day_one = Read_yaml.yaml_file schedule_of_yaml "2024/schedule/16-09.yaml"
+  let day_one =
+    Read_yaml.yaml_file schedule_of_yaml "2024/schedule/16-09.yaml"
     |> Read_yaml.Result.get_ok ~error:(fun (`Msg m) -> Invalid_argument m)
 
-  let day_two = Read_yaml.yaml_file schedule_of_yaml "2024/schedule/17-09.yaml"
+  let day_two =
+    Read_yaml.yaml_file schedule_of_yaml "2024/schedule/17-09.yaml"
     |> Read_yaml.Result.get_ok ~error:(fun (`Msg m) -> Invalid_argument m)
-
-end 
+end
